b93546cbcf706e3d0b49a8b77f58a945e9508b60
shawn <63920977+shawn@users.noreply.github.com>
2021-10-09 18:04:20 -0500

    update index.js

--- index.js
+++ index.js
@@ -15,161 +15,71 @@ window.onload = () => {
 
   registerServiceWorker();
 };
-
-const content = document.querySelector(".content");
-const apts = [
-  {
-    aptName: "Acacia Park Apartments",
-    contactName: "Albert Melendez",
-    phone: "915-833-7408",
-    address: "5848 Acacia Cir",
-    city: "El Paso",
-    state: "TX",
-    zip: "79912",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13559.756862789352!2d-106.52849497195865!3d31.826648300307664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e757e9cb274d5b%3A0xae9defce309fa3b5!2sAcacia%20Park%20Apartments!5e0!3m2!1sen!2sus!4v1633399303641!5m2!1sen!2sus",
-    // nav:'https://goo.gl/maps/mbfehys8NevDbntK6',
-    nav3: "https://www.google.com/maps/dir//Acacia+Park+Apartments,+5848+Acacia+Cir,+El+Paso,+TX+79912/@31.8266486,-106.5219289,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e757e9cb274d5b:0xae9defce309fa3b5!2m2!1d-106.5197402!2d31.8266486",
-  },
-  {
-    aptName: "Desert Sky Townhomes",
-    contactName: "Jennifer Conley",
-    phone: "915-751-9924",
-    address: "9924 Railroad Dr",
-    city: "El Paso",
-    state: "TX",
-    zip: "79924",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13549.337269411764!2d-106.3914066!3d31.8975127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e7519dc9b41299%3A0xb81b4fc8c6af0130!2sDesert%20Sky%20Townhomes!5e0!3m2!1sen!2sus!4v1633398985405!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/fCYNQQHqq7ExRYgH6",
-    nav3: "https://www.google.com/maps/dir//Desert+Sky+Townhomes,+9924+Railroad+Dr,+El+Paso,+TX+79924/@31.8975127,-106.3914066,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e7519dc9b41299:0xb81b4fc8c6af0130!2m2!1d-106.3910719!2d31.8972055",
-  },
-  {
-    aptName: "High Ridge Apartments",
-    contactName: "Veronica Herrera",
-    phone: "915-598-0842",
-    address: "9353 Viscount",
-    city: "El Paso",
-    state: "TX",
-    zip: "79925",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13567.97567436472!2d-106.37157577198404!3d31.770651800598348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e75b3cb0113d49%3A0x5ebbc8ca6ac7c341!2sHigh%20Ridge%20Apartments!5e0!3m2!1sen!2sus!4v1633398978119!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/sVZXieo5bdCMJ9NK8",
-    nav3: "https://www.google.com/maps/dir//High+Ridge+Apartments,+9353+Viscount+Blvd,+El+Paso,+TX+79925/@31.7706521,-106.3650097,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e75b3cb0113d49:0x5ebbc8ca6ac7c341!2m2!1d-106.3629348!2d31.7706524",
-  },
-
-  {
-    aptName: "Mountain Vista Appartment",
-    contactName: "Gidget Flores",
-    phone: "915-751-5757",
-    address: "5757 Will Ruth",
-    city: "El Paso",
-    state: "TX",
-    zip: "79924",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13550.947001487297!2d-106.41577997193144!3d31.886573999997868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e75112dc533305%3A0x86137ab2c896755d!2sMountain%20Vista%20Apartments!5e0!3m2!1sen!2sus!4v1633398970539!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/DmXS8JDXPxzqUYJR6",
-    nav3: "https://www.google.com/maps/dir//Mountain+Vista+Apartments,+5757+Will+Ruth+Ave,+El+Paso,+TX+79924/@31.8865743,-106.4092139,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e75112dc533305:0x86137ab2c896755d!2m2!1d-106.4070241!2d31.886501",
-  },
-
-  {
-    aptName: "Shadow Ridge Apartments",
-    contactName: "Kristina Chaves",
-    phone: "915-593-5250",
-    address: "9375 Viscount Blvd",
-    city: "El Paso",
-    state: "TX",
-    zip: "79925",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13568.095345893204!2d-106.3716907719844!3d31.769835800602596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e75b3b719e0801%3A0xdb27f91de7843774!2sShadow%20Ridge%20Apartments!5e0!3m2!1sen!2sus!4v1633398961493!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/TSdaFJ9aaB7X1wRo8",
-    nav3: "https://www.google.com/maps/dir//Shadow+Ridge+Apartments,+9375+Viscount+Blvd,+El+Paso,+TX+79925/@31.7698361,-106.3651247,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e75b3b719e0801:0xdb27f91de7843774!2m2!1d-106.362936!2d31.7698361",
-  },
-
-  {
-    aptName: "The Chimineys Apartments",
-    contactName: "Laura Esquivel",
-    phone: "915-584-4323",
-    address: "200 North Festival",
-    city: "El Paso",
-    state: "TX",
-    zip: "79912",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13560.804005337539!2d-106.5196577719619!3d31.819518800344618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e75792ad7680f3%3A0x56e26bcb8db35795!2sThe%20Chimneys%20Apartments!5e0!3m2!1sen!2sus!4v1633398955324!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/V4oEZPB34bxRYw2BA",
-    nav3: "https://www.google.com/maps/dir//The+Chimneys+Apartments,+200+N+Festival+Dr,+El+Paso,+TX+79912/@31.8195191,-106.5130917,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e75792ad7680f3:0x56e26bcb8db35795!2m2!1d-106.5110101!2d31.8196146",
-  },
-
-  {
-    aptName: "The Patriot Apartments",
-    contactName: "Karen Gutierrez",
-    phone: "915-757-6901",
-    address: "4600 Fairbanks",
-    city: "El Paso",
-    state: "TX",
-    zip: "79924",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13548.752011993009!2d-106.44570717192465!3d31.90148889992097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e751548eabfab5%3A0xa47b692c4925f4d!2sThe%20Patriot%20Apartments!5e0!3m2!1sen!2sus!4v1633398927145!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/bGPAw9MZ3KymkQRS6",
-    nav3: "https://www.google.com/maps/dir//The+Patriot+Apartments,+4600+Fairbanks+Dr,+El+Paso,+TX+79924/@31.9014892,-106.4391411,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e751548eabfab5:0xa47b692c4925f4d!2m2!1d-106.4368934!2d31.9014218",
-  },
-
-  {
-    aptName: "The Phoenix Apartments",
-    contactName: "Bonnie Doblado",
-    phone: "915-595-5487",
-    address: "7401 Phoenix Ave.",
-    city: "El Paso",
-    state: "TX",
-    zip: "79915",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13570.21688794295!2d-106.37670407199097!3d31.755366600677885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e75b41f28e9387%3A0xbcb39aad38ba3ba1!2sThe%20Phoenix%20Apartments!5e0!3m2!1sen!2sus!4v1633398919882!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/KUq6DrePyi5NzDV56",
-    nav3: "https://www.google.com/maps/dir//The+Phoenix+Apartments,+7401+Phoenix+Ave,+El+Paso,+TX+79915/@31.7553669,-106.370138,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e75b41f28e9387:0xbcb39aad38ba3ba1!2m2!1d-106.367948!2d31.7554298",
-  },
-  {
-    aptName: "Van Horne Apartments",
-    contactName: "Lucia Sanchez",
-    phone: "915-821-0130",
-    address: "11611 Dyer St.",
-    city: "El Paso",
-    state: "TX",
-    zip: "79934",
-    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13542.269875550852!2d-106.37993037190465!3d31.945498499694512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e7514555555555%3A0xe35fde208a7ea5a0!2sVan%20Horne%20Estates%20Apartments!5e0!3m2!1sen!2sus!4v1633398908845!5m2!1sen!2sus",
-    nav: "https://goo.gl/maps/wMZunfs9yzWhaoz66",
-    nav2: "https://www.google.com/maps/place/Van+Horne+Estates+Apartments/@31.9454988,-106.3733643,17z/data=!3m1!4b1!4m5!3m4!1s0x86e7514555555555:0xe35fde208a7ea5a0!8m2!3d31.9454736!4d-106.3711443",
-    nav3: "https://www.google.com/maps/dir//Van+Horne+Estates+Apartments,+11611+Dyer+St,+El+Paso,+TX+79934/@31.9454988,-106.3733643,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x86e7514555555555:0xe35fde208a7ea5a0!2m2!1d-106.3711443!2d31.9454736",
-  },
-];
-
-function makeHTML(apt) {
-  const div = document.createElement("div");
-  div.innerHTML = `
-<div class="apartment">
-    <div class="info">
-        <div class="aptName">${apt.aptName}</div>
-        <div class="contactName">${apt.contactName}</div>
-        <div class="phoneNumber">
-            <a href="tel:+1${apt.phone.replaceAll("-", "")}">${apt.phone}</a>
-        
-        </div>
-        <div class="address">
-        ${apt.address}<br>
-        ${apt.city}, 
-        ${apt.state}&nbsp
-        ${apt.zip}
-        </div>
-    </div>
-    <div>
-    
-    ${apt.nav2 ? '<div class="nav"><a href="' + apt.nav2 + '">Navigate</a></div>' : ""}
-    ${apt.nav3 ? '<div class="nav"><a href="' + apt.nav3 + '">Navigate Current Location</a></div>' : ""}
-    <span>testing</span>
-    
-    <div class="showMap">Show Map</div>
-      <div class="map">
-          <iframe
-              src="${apt.map}"
-              style="border: 0"
-              allowfullscreen=""
-              loading="lazy"
-          ></iframe>
-      </div>
-    
-    </div>
-</div>
-`;
-  return div;
+"use strict";
+const inpInterestRate = document.getElementById("interestRate");
+const inpNumOfYears = document.getElementById("numOfYears");
+const inpLoanAmount = document.getElementById("loanAmount");
+const btnCalculate = document.getElementById("btnCalculate");
+const divMonthlyPayment = document.getElementById("monthlyPayment");
+const divTotalInterest = document.getElementById("totalInterest");
+const divTotalHouseCost = document.getElementById("totalHouseCost");
+const divBreakdown = document.getElementById("breakdown");
+const divMonthlyTaxes = document.getElementById("monthlyTaxes");
+const divMonthlyTotal = document.getElementById("monthlyTotal");
+btnCalculate.addEventListener("click", calculate);
+function calculate() {
+    let interestRate = parseFloat(inpInterestRate.value);
+    let monthlyInterestRate = interestRate / 100 / 12;
+    let yearsOfLoan = parseInt(inpNumOfYears.value);
+    let totalPayments = yearsOfLoan * 12;
+    let loanAmount = parseInt(inpLoanAmount.value);
+    let monthlyPayment = pmt(interestRate, yearsOfLoan, loanAmount);
+    console.log('loan amount>> ', loanAmount);
+    console.log('monthly payment >> ', monthlyPayment);
+    console.log('interest rate >> ', interestRate);
+    let totalInterest = calculateTotalInterest(loanAmount, yearsOfLoan, monthlyPayment);
+    let total = calculateTotalCost(totalInterest, loanAmount);
+    let monthlyTaxes = loanAmount * .03 / 12;
+    let monthlyTotal = monthlyTaxes + monthlyPayment;
+    divMonthlyPayment.innerText = `$${monthlyPayment.toFixed(2)}`;
+    divTotalInterest.innerText = `$${totalInterest.toFixed(2)}`;
+    divTotalHouseCost.innerText = `$${total.toFixed(2)}`;
+    divMonthlyTaxes.innerText = `${monthlyTaxes.toFixed(2)}`;
+    divMonthlyTotal.innerText = `$${monthlyTotal.toFixed(2)}`;
+    monthlyInterestBreakdown(1, monthlyPayment, monthlyInterestRate, 0, 0, loanAmount);
+}
+function monthlyInterestBreakdown(month, monthlyPayment, monthlyInterestRate, totalInterestPaid, totalPrincipalPaid, remainingPrincipal) {
+    const interestPayment = remainingPrincipal * monthlyInterestRate;
+    const principalPayment = monthlyPayment - interestPayment;
+    const newTotalInterestPaid = totalInterestPaid + interestPayment;
+    const newTotalPrincipalPaid = totalPrincipalPaid + principalPayment;
+    const newRemainingPrincipal = remainingPrincipal - principalPayment;
+    const tr = newTableRow(month, monthlyPayment, interestPayment, newTotalInterestPaid, principalPayment, newTotalPrincipalPaid, newRemainingPrincipal);
+    if (newRemainingPrincipal > 0) {
+        monthlyInterestBreakdown(month + 1, monthlyPayment, monthlyInterestRate, newTotalInterestPaid, newTotalPrincipalPaid, newRemainingPrincipal);
+    }
+}
+function newTableRow(month, monthlyPayment, interestPayment, totalInterestPaid, principalPayment, totalPrincipalPaid, remainingPrincipal) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+          <td>${month}</td>
+          <td>${monthlyPayment.toFixed(2)}</td>
+          <td>${interestPayment.toFixed(2)}</td>
+          <td>${totalInterestPaid.toFixed(2)}</td>
+          <td>${principalPayment.toFixed(2)}</td>
+          
+          <td>${totalPrincipalPaid.toFixed(2)}</td>
+          <td>${remainingPrincipal.toFixed(2)}</td>
+          `;
+    divBreakdown.append(tr);
+    return tr;
+}
+function pmt(interestRate, numOfYears, loanAmount) {
+    return ((interestRate / 100 / 12) * loanAmount) / (1 - (1 + (interestRate / 100 / 12)) ** (-numOfYears * 12));
+}
+function calculateTotalInterest(loanAmount, numOfYears, monthlyPayment) {
+    return (loanAmount - numOfYears * 12 * monthlyPayment) * -1;
+}
+function calculateTotalCost(totalInterest, totalLoanAmount) {
+    return totalInterest + totalLoanAmount;
 }
